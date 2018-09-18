4b1c5600921aef55a7d2df6a7dbb9132cf7367c9
azhour <azhour.zoubir@gmail.com>
2018-09-18 11:56:53 +0200

    code

--- minichat.php
+++ minichat.php
@@ -18,7 +18,7 @@
   include('../divers/divers2.php');
   try
   {
-      $bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', $mdp);
+      $bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', '');
       $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }
   catch(Exception $e)
